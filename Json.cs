4fc72040e3c48864db925291e24c6cf31cbd9a8d
DoubleTurtle <98425496+GoldenxSun@users.noreply.github.com>
2022-07-18 21:18:33 +0200

    Update Json.cs

--- Json.cs
+++ Json.cs
@@ -1,37 +1,37 @@
+using System;
 using System.Text.Json;
-using System.IO;
 
-public class Json
+static internal class Json
 {
     // json at exe directory
-    public void writeJsonAtExe<T>(T content, string fileName) => writeAtExe(JsonSerializer.Serialize<T>(content), fileName);
-    private void writeAtExe(string content, string fileName) => File.WriteAllText($"{System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location)}/{fileName}.json", content);
+    static public void WriteJsonAtExe<T>(T content, string fileName) => WriteAtExe(JsonSerializer.Serialize<T>(content), fileName);
+    static private void WriteAtExe(string content, string fileName) => File.WriteAllText($"{System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location)}/{fileName}.json", content);
 
-    public bool readJsonAtExe<T>(ref T myObject, string fileName) 
-    { 
+    static public bool ReadJsonAtExe<T>(ref T myObject, string fileName)
+    {
         FileInfo _fileName = new FileInfo($"{System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location)}/{fileName}.json");
-        if (_fileName.Exists) 
-        { 
-            myObject = JsonSerializer.Deserialize<T>(readAtExe(fileName)); 
+        if (_fileName.Exists)
+        {
+            myObject = JsonSerializer.Deserialize<T>(ReadAtExe(fileName));
         }
         return _fileName.Exists;
     }
 
-    private string readAtExe(string fileName) => File.ReadAllText($"{System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location)}/{fileName}.json"); 
+    static private string ReadAtExe(string fileName) => File.ReadAllText($"{System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location)}/{fileName}.json");
 
     // json at specific path
-    public void writeJsonAtPath<T>(T content, string path) => writeAtPath(JsonSerializer.Serialize<T>(content), path);
-    private void writeAtPath(string content, string path) => File.WriteAllText($"{path}.json", content);
+    static public void WriteJsonAtPath<T>(T content, string path) => WriteAtPath(JsonSerializer.Serialize<T>(content), path);
+    static private void WriteAtPath(string content, string path) => File.WriteAllText($"{path}.json", content);
 
-    public bool readJsonAtPath<T>(ref T myObject, string path) 
-    { 
+    static public bool ReadJsonAtPath<T>(ref T myObject, string path)
+    {
         FileInfo _path = new FileInfo($"{path}.json");
-        if (_path.Exists) 
-        { 
-            myObject = JsonSerializer.Deserialize<T>(readAtPath(path)); 
+        if (_path.Exists)
+        {
+            myObject = JsonSerializer.Deserialize<T>(ReadAtPath(path));
         }
         return _path.Exists;
     }
 
-    private string readAtPath(string path) => File.ReadAllText($"{path}.json"); 
+    static private string ReadAtPath(string path) => File.ReadAllText($"{path}.json");
 }
